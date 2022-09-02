30a40ed43226d81bb0d4a0d00d40d43208023853
Sergey <thextreme.30@gmail.com>
2022-09-03 00:43:39 +0600

    user serializer

--- oauth/serializer.py
+++ oauth/serializer.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
 
+from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AuthUser
+        fields = ('avatar', 'country', 'city', 'bio', 'username')
+
 
 class GoogleAuth(serializers.Serializer):
     email = serializers.EmailField()
