3da6455d9acc51c88d839d82eb9e3abbb6d3d60f
Sergey <thextreme.30@gmail.com>
2022-09-05 17:35:45 +0600

    created sound serializers.

---
+++ sound/serializers.py
@@ -0,0 +1,76 @@
+from rest_framework import serializers
+
+from oauth.serializers import AuthorSerializer
+
+from . import models
+from .services.services import delete_old_file
+
+
+class BaseSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+
+
+class GenreSerializer(BaseSerializer):
+    class Meta:
+        model = models.Genre
+        fields = ('id', 'name')
+
+
+class AlbumSerializer(BaseSerializer):
+    class Meta:
+        model = models.Album
+        fields = ('id', 'name', 'description', 'cover', 'private')
+
+    def update(self, instance, validated_data):
+        delete_old_file(instance.cover.path)
+        return super().update(instance, validated_data)
+
+
+class CreateTitleSerializer(BaseSerializer):
+    plays_count = serializers.IntegerField(read_only=True)
+    download = serializers.IntegerField(read_only=True)
+    user = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = models.Title
+        fields = ('id', 'name', 'genre', 'album', 'link_of_author', 'file', 'create_at',
+                  'plays_count', 'download', 'private', 'cover', 'user')
+
+    def update(self, instance, validated_data):
+        delete_old_file(instance.file.path)
+        delete_old_file(instance.cover.path)
+        return super().update(instance, validated_data)
+
+
+class TitleSerializer(CreateTitleSerializer):
+    genre = GenreSerializer(many=True)
+    album = AlbumSerializer()
+    user = AuthorSerializer()
+
+
+class CreatePlaylistSerializer(BaseSerializer):
+    class Meta:
+        model = models.PlayList
+        fields = ('id', 'name', 'cover', 'titles')
+
+    def update(self, instance, validated_data):
+        delete_old_file(instance.cover.path)
+        return super().update(instance, validated_data)
+
+
+class PlaylistSerializer(CreatePlaylistSerializer):
+    titles = TitleSerializer(many=True)
+
+
+class CommentAuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'text', 'title')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = AuthorSerializer()
+
+    class Meta:
+        model = models.Comment
+        fields = ('id', 'text', 'user', 'title', 'create_at')
--- sound/services/services.py
+++ sound/services/services.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.exceptions import ValidationError
 
 
@@ -12,13 +14,13 @@ def get_playlist_cover_upload_path(instance, file):
 
 
 def get_title_upload_path(instance, file):
-    """Построение пути к файлу, format: (media)/track/user_id/audio.pm3."""
-    return f'track/user_{instance.user.id}/{file}'
+    """Построение пути к файлу, format: (media)/title/user_id/audio.pm3."""
+    return f'title/user_{instance.user.id}/{file}'
 
 
 def get_title_cover_upload_path(instance, file):
-    """Построение пути к файлу, format: (media)/track/cover/user_id/photo.jpg."""
-    return f'track/cover/user_{instance.user.id}/{file}'
+    """Построение пути к файлу, format: (media)/title/cover/user_id/photo.jpg."""
+    return f'title/cover/user_{instance.user.id}/{file}'
 
 
 def validate_size_image(file_obj):
@@ -26,3 +28,8 @@ def validate_size_image(file_obj):
     megabyte_limit = 4
     if file_obj.size > megabyte_limit * 1024 * 1024:
         raise ValidationError(f"Максимальный размер файла {megabyte_limit}MB")
+
+def delete_old_file(path_file):
+    """Удаление старого файла."""
+    if os.path.exists(path_file):
+        os.remove(path_file)
