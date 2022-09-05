187fc8561e0ca259b6b0a6822e46d566d7960744
Sergey <thextreme.30@gmail.com>
2022-09-05 18:51:16 +0600

    created sound serializers

--- sound/serializers.py
+++ sound/serializers.py
@@ -50,7 +50,7 @@ class TitleSerializer(CreateTitleSerializer):
 
 class CreatePlaylistSerializer(BaseSerializer):
     class Meta:
-        model = models.PlayList
+        model = models.Playlist
         fields = ('id', 'name', 'cover', 'titles')
 
     def update(self, instance, validated_data):
@@ -74,3 +74,13 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = ('id', 'text', 'user', 'title', 'create_at')
+
+
+class MixedSerializer:
+    def get_serializer(self, *args, **kwargs):
+        try:
+            serializer_class = self.serializer_classes_by_action[self.action]
+        except KeyError:
+            serializer_class = self.get_serializer_class()
+        kwargs.setdefault('context', self.get_serializer_context())
+        return serializer_class(*args, **kwargs)
