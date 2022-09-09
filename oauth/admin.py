a654950269f5ef7e676e2c8c8459c73bc635300a
Sergey <thextreme.30@gmail.com>
2022-09-09 17:54:24 +0600

    oauth changed custom user model

--- oauth/admin.py
+++ oauth/admin.py
@@ -1,9 +1,67 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-from . import models
+from .models import CustomUser
 
 
-@admin.register(models.AuthUser)
-class AuthUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'username', 'join_date')
-    list_display_links = ('email',)
+class UserCreationForm(forms.ModelForm):
+
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'join_date')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Пароли не совпадают")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'join_date', 'is_active', 'is_admin')
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'join_date', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('join_date',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'join_date', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(CustomUser, UserAdmin)
+
+admin.site.unregister(Group)
--- oauth/models.py
+++ oauth/models.py
@@ -1,10 +1,36 @@
 from django.core.validators import validate_image_file_extension
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
 from oauth.services.services import get_avatar_upload_path, validate_image_size
 
 
-class AuthUser(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, join_date, password=None):
+        if not email:
+            raise ValueError('Пользователи должны иметь адрес электронной почты')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            join_date=join_date,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, join_date, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            join_date=join_date,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
     """Модель пользователя."""
     email = models.EmailField(max_length=256, unique=True)
     join_date = models.DateTimeField(auto_now_add=True)
@@ -18,6 +44,14 @@ class AuthUser(models.Model):
         validators=[validate_image_file_extension, validate_image_size],
     )
 
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['join_date']
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
@@ -25,24 +59,12 @@ class AuthUser(models.Model):
     def __str__(self):
         return self.email
 
-    @property
-    def is_authenticated(self):
+    def has_perm(self, perm, obj=None):
         return True
 
+    def has_module_perms(self, app_label):
+        return True
 
-class Follower(models.Model):
-    """Модель подписок."""
-    user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='owner')
-    subscriber = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='subscriber')
-
-    class Meta:
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'subscriber'], name='follow_unique'
-            )
-        ]
-
-    def __str__(self):
-        return f'{self.subscriber} подписан на {self.user}'
+    @property
+    def is_staff(self):
+        return self.is_admin
